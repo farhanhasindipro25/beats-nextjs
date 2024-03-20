690969179e38e32e59f43cc16c9f6dd04fea4c3f
farhanhasindipro25 <farhan.hasin.25@gmail.com>
2024-03-21 04:44:15 +0600

    feat: Added recommended page data

--- app/recommended/page.jsx
+++ app/recommended/page.jsx
@@ -1,5 +1,22 @@
+"use client";
 import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import getMusicList from "../_libs/services/getMusicList";
+import RecommendedListView from "./_libs/component/RecommendedListView";
 
 export default function RecommendedPage() {
-  return <div>RecommendedPage</div>;
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["songs"],
+    queryFn: () => getMusicList(),
+  });
+  return (
+    <div className="space-y-6">
+      <h2 className="text-xl font-semibold text-white">Recommended Songs</h2>
+      <RecommendedListView
+        data={data}
+        isLoading={isLoading}
+        isError={isError}
+      />
+    </div>
+  );
 }
